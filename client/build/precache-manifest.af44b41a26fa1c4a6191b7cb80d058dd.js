4f1867fdffc69b86d60b9389ded0a6a137787665
Alexy Kotliar <kotliar.alex@gmail.com>
2021-07-25 17:42:11 -0400

    update before upload to heroku

---
+++ client/build/precache-manifest.af44b41a26fa1c4a6191b7cb80d058dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "868169867faebcfcf6390b5c665d5242",
+    "url": "/index.html"
+  },
+  {
+    "revision": "400babf49c1c651c84d6",
+    "url": "/static/css/main.e213c893.chunk.css"
+  },
+  {
+    "revision": "ba0a54a4df7e348063c4",
+    "url": "/static/js/2.33da7d04.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.33da7d04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "400babf49c1c651c84d6",
+    "url": "/static/js/main.830fb13f.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
